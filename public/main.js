b843e71f8cb0c3a8ee194050b034d097bc5801e1
Ahmad Al-Tarifi <70255198+AhmadAltarifi@users.noreply.github.com>
2021-08-30 00:57:28 +0300

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,43 @@
+function myFunction() {
+    var x = document.getElementById("mynavbarr");
+    if (x.className === "navbarr") {
+        x.className += " responsive";
+
+
+    } else {
+        x.className = "navbarr";
+
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("mynavbarr");
+    if (x.className === "navbarr") {
+        x.className += " responsive";
+        document.getElementById("myCarousel").style.marginTop = "340px";
+        document.getElementById("slideshow-containerr").style.marginTop = "340px";
+
+
+    } else {
+        x.className = "navbarr";
+        document.getElementById("myCarousel").style.marginTop = "0px";
+        document.getElementById("slideshow-containerr").style.marginTop = "0px";
+
+
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("mynavbarr");
+    if (x.className === "navbarr") {
+        x.className += " responsive";
+        document.getElementById("slideshow-containerr").style.marginTop = "340px";
+
+
+    } else {
+        x.className = "navbarr";
+        document.getElementById("slideshow-containerr").style.marginTop = "0px";
+
+
+    }
+}
